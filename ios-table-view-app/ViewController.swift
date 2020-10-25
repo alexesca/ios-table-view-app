24c7b5e490ad8108ee863380adf6810f88d37f91
alexesca <alexescamore@gmail.com>
2020-10-24 19:52:02 -0600

    Created io table view example app

--- ios-table-view-app/ViewController.swift
+++ ios-table-view-app/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let names = [
+        "Miguel Escamilla",
+        "Nancy Escamilla",
+        "Lorena Escamilla"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)  {
+        print("You tapped me!!!")
+    }
 
 }
 
+
+
+extension ViewController: UITableViewDelegate{
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell;
+    }
+}
